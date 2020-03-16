e64ee840f57ca1567e5e496aefc701e6bd49a0ed
Andrei Panov <panov.andy@gmail.com>
2020-03-16 22:30:58 +0100

    ssr2

--- hyoo/doc/server.node.ts
+++ hyoo/doc/server.node.ts
@@ -2,6 +2,6 @@
 const express = $node['express']()
 express.use((req, res, next) => {
 	$mol_fiber_root( ()=> $mol_fiber_unlimit( ()=> {
-		res.send(new $app_start().toString())
+		res.send(new $hyoo_doc().toString())
 	} ) )
 })
