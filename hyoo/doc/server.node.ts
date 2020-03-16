3a9c7a2c4e141aa6eb697fa45bb2b167320ba2cc
Andrei Panov <panov.andy@gmail.com>
2020-03-16 22:56:27 +0100

    fix namespace

--- hyoo/doc/server.node.ts
+++ hyoo/doc/server.node.ts
@@ -1,9 +1,9 @@
-import $mol_fiber_unlimit = $.$mol_fiber_unlimit;
-import $mol_fiber_root = $.$mol_fiber_root;
+namespace $ {
 
-export function $hyoo_doc_server(args: string[]) {
-	const app = $node['express']()
-	console.log(app)
+	export function $hyoo_doc_server(args: string[]) {
+		const app = $node['express']()
+		console.log(app)
+	}
+
+	setTimeout($mol_fiber_root(() => $mol_fiber_unlimit(() => $hyoo_doc_server(process.argv.slice(2)) as any)))
 }
-setTimeout( $mol_fiber_root( ()=> $mol_fiber_unlimit( ()=> $hyoo_doc_server( process.argv.slice( 2 ) ) as any ) ) )
-
